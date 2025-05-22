a56880dfebbe1fd3c9096a59d2f4308748248c2c
irfanalarib <irfanalarib7@gmail.com>
2025-05-22 09:59:24 +0700

    Set pointer next newNode ke top

--- file_1.cpp
+++ file_1.cpp
@@ -26,5 +26,6 @@ class Stack {
     int push(int value){
         node* newNode =new node();//1. allocate memory for the new node
         newNode->data = value; //2. assign value
+        newNode->next = top; //3. set the next pointer of the new node to the current top
     }
 };
